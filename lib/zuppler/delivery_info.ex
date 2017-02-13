7bc595cfa562c9b50db710368aa1735fdf448171
Silviu Rosu <silviu.rosu@gmail.com>
2017-02-13 13:40:49 +0200

    add functions for multiple param types

--- lib/zuppler/delivery_info.ex
+++ lib/zuppler/delivery_info.ex
@@ -6,7 +6,19 @@ defmodule Zuppler.DeliveryInfo do
                          charge_amount: float, charge_percent: float, time: integer}
 
   @spec find(integer, integer) :: {:ok, t} | {:error, String.t}
-  def find(restaurant_id, address_id) do
+  def find(restaurant_id, address_id) when is_integer(restaurant_id) and is_integer(address_id), do:
+    find(Integer.to_string(restaurant_id), Integer.to_string(address_id))
+
+  @spec find(integer, String.t) :: {:ok, t} | {:error, String.t}
+  def find(restaurant_id, address_id) when is_integer(restaurant_id) and is_binary(address_id), do:
+    find(Integer.to_string(restaurant_id), address_id)
+
+  @spec find(String.t, integer) :: {:ok, t} | {:error, String.t}
+  def find(restaurant_id, address_id) when is_binary(restaurant_id) and is_integer(address_id), do:
+    find(restaurant_id, Integer.to_string(address_id))
+
+  @spec find(String.t, String.t) :: {:ok, t} | {:error, String.t}
+  def find(restaurant_id, address_id) when is_binary(restaurant_id) and is_binary(address_id) do
     headers = ["Content-type": "application/json"]
     url = check_address_url(restaurant_id, address_id)
     Logger.info "Loading delivery rule settings from #{url}"
@@ -29,12 +41,22 @@ defmodule Zuppler.DeliveryInfo do
     end
   end
 
-  @spec check_address_url(integer, integer) :: String.t
+  @spec check_address_url(String.t, String.t) :: String.t
   defp check_address_url(restaurant_id, address_id) do
     config = Application.get_env(:zuppler_elixir, Zuppler.Endpoint)
+    restaurant_url = config[:restaurants_url]
+    check_address_endpoint = config[:check_address_endpoint]
+
+    if is_nil(restaurant_url) do
+      raise ArgumentError, message: "Restaurant url is not set. Please add it in config file"
+    end
+    if is_nil(check_address_endpoint) do
+      raise ArgumentError, message: "Address endpoint url is not set. Please add it in config file"
+    end
+
     url = config[:restaurants_url] <> config[:check_address_endpoint]
     url
-    |> String.replace(":restaurant_id", Integer.to_string(restaurant_id))
-    |> String.replace(":address_id", Integer.to_string(address_id))
+    |> String.replace(":restaurant_id", restaurant_id)
+    |> String.replace(":address_id", address_id)
   end
 end
